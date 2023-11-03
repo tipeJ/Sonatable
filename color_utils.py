b1f043368e29422c9a670a0ed6b87986e64f4968
tipej <tiitus.jarvinen@gmail.com>
2023-11-03 17:17:40 +0200

    Updated lerp to RGBW/RGB

--- color_utils.py
+++ color_utils.py
@@ -142,4 +142,10 @@ def wheel(wheel_pos):
         return (0, wheel_pos * 3, 255 - wheel_pos * 3)
 
 def lerp(color1, color2, t):
-    return (color1[0] + (color2[0] - color1[0]) * t, color1[1] + (color2[1] - color1[1]) * t, color1[2] + (color2[2] - color1[2]) * t)
+    # Check whether the colors are RGB or RGBW.
+    if len(color1) == 3 and len(color2) == 3:
+        return (color1[0] + (color2[0] - color1[0]) * t, color1[1] + (color2[1] - color1[1]) * t, color1[2] + (color2[2] - color1[2]) * t)
+    elif len(color1) == 4 and len(color2) == 4:
+        return (color1[0] + (color2[0] - color1[0]) * t, color1[1] + (color2[1] - color1[1]) * t, color1[2] + (color2[2] - color1[2]) * t, color1[3] + (color2[3] - color1[3]) * t)
+    else:
+        raise ValueError("Both colors must be either RGB or RGBW.")
