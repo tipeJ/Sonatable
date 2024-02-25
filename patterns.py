59a485f713285f18c64d98453b6bea8b4cc95d59
tipej <tiitus.jarvinen@gmail.com>
2024-02-25 22:49:54 +0200

    Refactored patterns

--- patterns.py
+++ patterns.py
@@ -1,7 +1,6 @@
 from neopixel import Neopixel
 from machine import Pin
 import time
-from main import LEDSTRIP_PIN
 import uasyncio as asyncio
 from micropython import const
 from color_utils import hsi2rgbw, RGBToRGBW, RGB2RGBW, lerp, random_rgb, wheel, beatsin88, beatsin16, beatsin8, beat16, sin16, scale16, getAverageLightness
@@ -22,7 +21,7 @@ _REED = const(70)
 class NeopixelConfigurationInterface:
     def __init__(self):
         self.NUM_PIXELS = 151
-        self.NP = Neopixel(self.NUM_PIXELS, 0, LEDSTRIP_PIN, "RGBW")
+        self.NP = Neopixel(self.NUM_PIXELS, 0, 22, "RGBW")
         # Set up mask for brightness. 0-255 value for each pixel
         self.brightness_mask = [255] * self.NUM_PIXELS
 
@@ -193,92 +192,6 @@ class NeopixelGradientPulseConfiguration(NeopixelConfigurationInterface):
                 self.fill(lerp(self.color2, self.color1, i / self.steps))
                 self.NP.show()
                 await asyncio.sleep_ms(self.wait_ms)
-
-class Pacifica(NeopixelConfigurationInterface):
-    # By https://gist.github.com/kriegsman/36a1e277f5b4084258d9af1eae29bac4
-    # Converted to Python by me :)
-    def __init__(self, wait_ms=50, palettes=[]):
-        self.wait_ms = wait_ms
-        self.palettes = palettes
-        self.sCIStart1, self.sCIStart2, self.sCIStart3, self.sCIStart4 = 0, 0, 0, 0
-        self.sLastms = 0
-        super().__init__()
-
-    @staticmethod
-    def from_json(json):
-        return Pacifica(json["wait_ms"], json["palettes"])
-    
-    async def setup(self):
-        self.fill((0, 0, 0, 0))
-        self.NP.show()
-        await asyncio.sleep_ms(1)
-
-    async def terminate(self):
-        self.fill((0, 0, 0, 0))
-        self.NP.show()
-        await asyncio.sleep_ms(1)
-
-    @staticmethod
-    async def loop(self):
-        # Color shift the fill to the other color, one step at a time. After the last step, start over.
-        while True:
-            await asyncio.sleep_ms(self.wait_ms)
-            self.pacifica_loop()
-            self.NP.show()
-
-    def pacifica_loop(self):
-        ms = time.ticks_ms()
-        deltams = time.ticks_diff(ms, self.sLastms)
-        self.lastms = ms
-        
-        speedfactor1 = beatsin16(3, 179, 269)
-        speedfactor2 = beatsin16(4, 179, 269)
-        deltams1 = (deltams * speedfactor1) // 256
-        deltams2 = (deltams * speedfactor2) // 256
-        deltams21 = (deltams1 + deltams2) // 2
-        self.sCIStart1 += (deltams1 * beatsin88(1011, 10, 13))
-        self.sCIStart2 += (deltams21 * beatsin88(777, 8, 11))
-        self.sCIStart3 += (deltams1 * beatsin88(501, 5, 7))
-        self.sCIStart4 += (deltams2 * beatsin88(257, 4, 6))
-
-        # Clear out the LED array to a dim background blue-green
-        self.fill((0, 0, 1, 1))
-
-        # Render each of four layers of waves, with different scales and speeds, that vary over time
-        self.one_layer_of_pacifica(self.palettes[0], self.sCIStart1,  beatsin16(3, 11 * 256, 14 * 256), beatsin8(10, 70, 130), 0-beat16(301))
-        self.one_layer_of_pacifica(self.palettes[1], self.sCIStart2,  beatsin16(4,  6 * 256, 9 * 256), beatsin8(17, 40, 80), beat16(401))
-        self.one_layer_of_pacifica(self.palettes[2], self.sCIStart3,  6 * 256, beatsin8(9, 10, 38), 0-beat16(503))
-        self.one_layer_of_pacifica(self.palettes[3], self.sCIStart4,  5 * 256, beatsin8(8, 10, 28), beat16(601))
-
-
-    def one_layer_of_pacifica(self, palette, cistart, wavescale, bri, ioff):
-
-        wavescale_half = wavescale // 2 + 20
-        waveangle = ioff
-        ci = cistart
-        for i in range(0, self.NUM_PIXELS):
-            waveangle += 250
-            s16 = sin16(waveangle) + 32768
-            cs = scale16(s16, wavescale_half) + wavescale_half
-            ci += cs
-            sindex16 = sin16(ci) + 32768
-            sindex8 = scale16(sindex16, 240)
-            color = palette[sindex8]
-            self.set_pixel(i, hsi2rgbw(color, 255, bri, 255))
-
-    def add_whitecaps(self):
-        basethreshold = beatsin8(9, 55, 65)
-        wave = beat16(7)
-
-        for i in range(0, self.NUM_PIXELS):
-            threshold = scale16(sin16(wave), 20) + basethreshold
-            wave += 300
-            l = self.get_pixel_color(i)
-            li = getAverageLightness(l)
-            if li > threshold:
-                overage = li - threshold
-                overage2 = min(overage + overage, 255)
-                self.set_pixel(i, (overage, overage2, min(overage2 + overage2, 255), 0))
 
 class Rainbow(NeopixelConfigurationInterface):
     def __init__(self, wait_ms=50):
